4e089d601f2b204aa44d8574ff9a516819f36ffc
Ashwin Sayenju <ashwinsayenju@Ashwins-MacBook-Air.local>
2026-04-10 21:27:44 -0400

    Removed unused variable

--- src/main/java/com/nepalaccountant/backend/controller/BusinessController.java
+++ src/main/java/com/nepalaccountant/backend/controller/BusinessController.java
@@ -11,7 +11,6 @@ import jakarta.validation.Valid;
 import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
